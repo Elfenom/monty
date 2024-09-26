9c335ba825c71ab8cd10c56b4b2ea5d1c37807ec
Elfenom <elphenom10@gmail.com>
2024-09-26 12:12:27 -0700

    0. push, pall

---
+++ _push.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * push - push value to top of stack
+ * @stack: pointer to top of stack
+ * @val: value to push to stack
+ */
+void push(stack_t **stack, unsigned int val)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+	{
+		fprintf(stderr, "%s\n", "Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+	node->n = val;
+	node->prev = *stack;
+	node->next = NULL;
+	if (*stack != NULL)
+		(*stack)->next = node;
+	*stack = node;
+}
---
+++ monty.h
@@ -0,0 +1,54 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+void add(stack_t **stack, unsigned int ln);
+void pall(stack_t **stack, unsigned int ln);
+void pint(stack_t **stack, unsigned int ln);
+void pop(stack_t **stack, unsigned int ln);
+void push(stack_t **stack, unsigned int val);
+void swap(stack_t **stack, unsigned int ln);
+void nop(stack_t **stack, unsigned int ln);
+int _getline(char **lineptr, int fd, unsigned int *);
+void load_opcodes(instruction_t opcodes[], int size);
+void get_optstr(char **opstr, char *line);
+void f_exec(instruction_t [], unsigned int, int, char *);
+
+extern stack_t *stack;
+extern unsigned int stack_size;
+#endif
